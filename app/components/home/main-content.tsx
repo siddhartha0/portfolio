029d7ccce17489fc1c55e1e3397ce4335157f40c
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-11-14 11:12:57 +0545

    main content edited

--- app/components/home/main-content.tsx
+++ app/components/home/main-content.tsx
@@ -11,14 +11,14 @@ export const MainContent = () => {
       </header>
       <div className="relative">
         <motion.h2
-          className="text-8xl font-bold leading-tight mb-4"
+          className="text-8xl uppercase font-bold leading-tight mb-4"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
         >
-          Brand and
+          Skilled and
           <br />
-          Design
+          Innovative
         </motion.h2>
         <motion.p
           className="text-base mb-8 mt-14"
@@ -31,19 +31,6 @@ export const MainContent = () => {
           <br />I specialize in creating efficient, scalable, and visually
           appealing web applications. Let’s turn your ideas into reality!”
         </motion.p>
-        <motion.div
-          className="absolute right-0 bottom-0"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.8, duration: 0.8 }}
-        >
-          <Image
-            src="/placeholder.svg?height=300&width=300"
-            alt="Chess pieces"
-            width={300}
-            height={300}
-          />
-        </motion.div>
       </div>
       <motion.div
         className="absolute top-12 right-12 bg-white text-black p-6 rounded-lg shadow-lg max-w-xs"
@@ -61,19 +48,27 @@ export const MainContent = () => {
           Tools: Git, GitHub, Postman, VS Code
         </p>
       </motion.div>
-      <div className="absolute bottom-12 right-12 flex space-x-2">
-        {["Database", "Backend", "Frontend"].map((skill, index) => (
-          <motion.div
-            key={skill}
-            className="bg-gray-800 text-white text-xs px-3 py-1 rounded-full"
-            initial={{ opacity: 0, x: 20 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ delay: 1.6 + index * 0.1, duration: 0.5 }}
-          >
-            {skill}
-          </motion.div>
-        ))}
-      </div>
+
+      <motion.div
+        className="absolute right-0 bottom-0"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.8, duration: 0.8 }}
+      >
+        <div className="absolute bottom-12 right-12 flex space-x-2">
+          {["Database", "Backend", "Frontend"].map((skill, index) => (
+            <motion.div
+              key={skill}
+              className="bg-gray-800 text-white text-xs px-3 py-1 rounded-full"
+              initial={{ opacity: 0, x: 20 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ delay: 1.6 + index * 0.1, duration: 0.5 }}
+            >
+              {skill}
+            </motion.div>
+          ))}
+        </div>
+      </motion.div>
     </main>
   );
 };
