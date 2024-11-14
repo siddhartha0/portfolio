7f722a35924969e0066e0ddf4c50bce7a3206617
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-11-14 11:28:50 +0545

    changes

--- app/components/home/secondary-content.tsx
+++ app/components/home/secondary-content.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import { Github, Linkedin, Mail } from "lucide-react";
